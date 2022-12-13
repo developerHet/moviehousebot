53a4d85cd837d57fa87455cb4bfdabde91793762
developerHet <hetpatel2312.hp@gmail.com>
2022-12-13 13:23:31 +0530

    Beutify Message

--- bot.js
+++ bot.js
@@ -9,34 +9,53 @@ dotenv.config({ path: "./config/config.env" });
 const token = process.env.TELEGRAM_BOT_API_KEY;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {
-  webHook: {
+
+var bot;
+if (process.env.NODE_ENV === "production") {
+  bot = new TelegramBot(token, {
+    webHook: {
       port: process.env.PORT,
-      host: "0.0.0.0"
-  }
-});
+      host: "0.0.0.0",
+    },
+  });
 
-bot.setWebHook(
-  `${process.env.HOST_URL}/${token}`
-);
+  bot.setWebHook(`${process.env.HOST_URL}/${token}`);
+  console.log(`Bot running on port ${process.env.PORT}`);
+} else {
+  bot = new TelegramBot(token, { polling: true });
+  console.log("Bot running in development mode");
+}
 
-console.log("Bot running on server....");
+const startMessage = `<b>Request Message Format</b>\n\nMovie_Name/Quality\nEx. Iron Man/720p\n\nSeries_Name/Quality/s1/e1\nEx. Money Heist/720p/s1/e1 \n\n<b>Note:</b>\nTake movie name from google to avoid spelling mistake 🔎.`;
 
 bot.on("message", async (msg) => {
   const userChatId = msg.chat.id;
   const userMessage = msg.text;
-  
-  if ((msg.video || msg.document) && userChatId===process.env.USER_CHAT_ID) {
-    await bot.sendMessage(userChatId, msg.video.file_id);
+  const userName = msg.from.username;
+  console.log(msg);
+
+  if( !(msg.video || msg.document) && userMessage.startsWith('/')) {
+    await bot.sendMessage(
+      userChatId,
+      startMessage,
+      {parse_mode : "HTML"}
+    );
     return;
   }
 
-  if (userMessage) {
-    // if (!userMessage.startsWith("tt")) {
-    //   await bot.sendMessage(userChatId, "Please enter valid IMDB ID.");
-    //   return;
-    // }
+  if (
+    (msg.video || msg.document) &&
+    (userChatId == process.env.USER_CHAT_ID_1 ||
+      userChatId == process.env.USER_CHAT_ID_2)
+  ) {
+    const fileIdWithName = `${msg.caption} :- \n\n <code>${msg.video.file_id}</code>`;
+    await bot.sendMessage(userChatId, fileIdWithName,{parse_mode: "HTML"});
+    return;
+  }
 
+  
+
+  if (userMessage) {
     const messageArray = userMessage.split("/");
 
     if (messageArray.length !== 2 && messageArray.length !== 4) {
@@ -54,14 +73,16 @@ bot.on("message", async (msg) => {
 
     const name = messageArray[0];
 
-    
-    const foundItem = data.find((element) => element.name.toLowerCase() == name.toLowerCase());
+    const foundItem = data.find(
+      (element) => element.name.toLowerCase() == name.toLowerCase()
+    );
 
     if (!foundItem) {
       await bot.sendMessage(
-        userChatId,
-        `${name} is not available`
+        process.env.USER_CHAT_ID_1,
+        `${name} is not available. @${userName}`
       );
+      await bot.sendMessage(userChatId, `${name} is not available`);
       return;
     }
 
@@ -81,7 +102,7 @@ bot.on("message", async (msg) => {
       return;
     }
 
-    const quality = messageArray[1];
+    const quality = messageArray[1].toLowerCase();
     const qualityArray = foundItem.data;
 
     const fileQuality = qualityArray.find(
@@ -90,20 +111,29 @@ bot.on("message", async (msg) => {
 
     if (!fileQuality) {
       await bot.sendMessage(
+        process.env.USER_CHAT_ID_1,
+        `${name} quality ${quality} is not available. @${userName}`
+      );
+      await bot.sendMessage(
         userChatId,
-        `File is not available in ${fileQuality}`
+        `File is not available in ${quality}`
       );
       return;
     }
 
     let fileId = fileQuality.fileId;
     if (messageArray.length === 2) {
-      await bot.sendDocument(userChatId, fileId, { caption: messageArray[0] });
+      const movieCaption = `Name :- ${messageArray[0]}\nQuality :- ${messageArray[1]}`;
+      await bot.sendDocument(userChatId, fileId, { caption: movieCaption });
       return;
     } else {
-      const seriesSeason = messageArray[2];
+      const seriesSeason = messageArray[2].toLowerCase();
 
       if (!(seriesSeason in fileQuality)) {
+        await bot.sendMessage(
+          process.env.USER_CHAT_ID_1,
+          `${name} season ${seriesSeason} is not available. @${userName}`
+        );
         await bot.sendMessage(
           userChatId,
           `${seriesSeason} season is not available`
@@ -113,9 +143,13 @@ bot.on("message", async (msg) => {
 
       const season = fileQuality[seriesSeason][0];
 
-      const seasonEpisode = messageArray[3];
+      const seasonEpisode = messageArray[3].toLowerCase();
 
       if (!(seasonEpisode in season)) {
+        await bot.sendMessage(
+          process.env.USER_CHAT_ID_1,
+          `${name} season ${seriesSeason}  episode ${seasonEpisode} is not available. @${userName}`
+        );
         await bot.sendMessage(
           userChatId,
           `${seasonEpisode} episode is not available`
@@ -124,8 +158,8 @@ bot.on("message", async (msg) => {
       }
 
       fileId = season[seasonEpisode];
-      const caption =  `Movie Name :- ${messageArray[0]}\nQuality :- ${messageArray[1]}`;
-      await bot.sendDocument(userChatId, fileId, { caption: caption });
+      const movieCaption = `Name :- ${messageArray[0]}\nQuality :- ${messageArray[1]}\nSeason :- Season ${seriesSeason[1]}\nEpisode :- Episode ${seasonEpisode[1]}`;
+      await bot.sendDocument(userChatId, fileId, { caption: movieCaption });
       return;
     }
   }
